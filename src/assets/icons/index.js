6f1e428ac4a6359fadaba1a41b1076c21c25fe8c
hoanglong161098 <1651012102hoanglong@gmail.com>
2023-11-12 20:49:33 +0700

    first commit

---
+++ src/assets/icons/index.js
@@ -0,0 +1,59 @@
+import css from './css.svg'
+import photoshop from './photoshop.svg'
+import git from './git.svg'
+import github from './github.svg'
+import html from './html.svg'
+import javascript from './javascript.svg'
+import figma from './figma.svg'
+import illustrator from './illustrator.svg'
+import mui from './mui.svg'
+import blender from './blender.svg'
+import react from './react.svg'
+import tailwindcss from './tailwindcss.svg'
+import typescript from './typescript.svg'
+import linkedin from './linkedin.svg'
+import twitter from './twitter.svg'
+import threads from './threads.svg'
+import arrow from './arrow.svg'
+import contact from './contact.svg'
+import soundon from './soundon.png'
+import soundoff from './soundoff.png'
+import tmt from './tmt.svg'
+import bearplus from './bearplus.svg'
+import tdental from './tdental.svg'
+import tpos from './tpos.svg'
+import sensorflow from './sensorflow.svg'
+import bluecarbon from './bluecarbon.png'
+import ybl from './ybl.svg'
+import long from './1long.svg'
+
+export {
+    css,
+    photoshop,
+    git,
+    github,
+    html,
+    javascript,
+    figma,
+    illustrator,
+    mui,
+    blender,
+    react,
+    tailwindcss,
+    typescript,
+    linkedin,
+    twitter,
+    threads,
+    arrow,
+    tmt,
+    bearplus,
+    tdental,
+    tpos,
+    sensorflow,
+    bluecarbon,
+    long,
+    ybl,
+    contact,
+    soundon,
+    soundoff
+}
---
+++ src/constants/index.js
@@ -0,0 +1,179 @@
+import {
+    bluecarbon,
+    contact,
+    css,
+    photoshop,
+    git,
+    github,
+    html,
+    javascript,
+    linkedin,
+    figma,
+    illustrator,
+    mui,
+    blender,
+    react,
+    tailwindcss,
+    tpos,
+    typescript,
+    tmt,
+    long,
+    ybl,
+    sensorflow,
+    tdental,
+    bearplus
+} from "../assets/icons";
+
+export const skills = [
+    {
+        imageUrl: figma,
+        name: "Figma",
+        type: "UI UX Design",
+    },
+    {
+        imageUrl: photoshop,
+        name: "Photoshop",
+        type: "Design",
+    },
+    {
+        imageUrl: illustrator,
+        name: "Illustrator",
+        type: "Design",
+    },
+    {
+        imageUrl: blender,
+        name: "Blender",
+        type: "3D Design",
+    },
+    {
+        imageUrl: html,
+        name: "HTML",
+        type: "Frontend",
+    },
+    {
+        imageUrl: css,
+        name: "CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: javascript,
+        name: "JavaScript",
+        type: "Frontend",
+    },
+    {
+        imageUrl: git,
+        name: "Git",
+        type: "Version Control",
+    },
+    {
+        imageUrl: github,
+        name: "GitHub",
+        type: "Version Control",
+    },
+    {
+        imageUrl: mui,
+        name: "Material-UI",
+        type: "Frontend",
+    },
+    {
+        imageUrl: react,
+        name: "React",
+        type: "Frontend",
+    },
+    {
+        imageUrl: tailwindcss,
+        name: "Tailwind CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: typescript,
+        name: "TypeScript",
+        type: "Frontend",
+    }
+];
+
+export const experiences = [
+    {
+        title: "UI UX Design",
+        company_name: "Truong Minh Thinh Technology Joint Stock Company",
+        icon: tmt,
+        iconBg: "#FADBDB",
+        date: "Deecember 2020 - June 2022",
+        points: [
+            "Participate in Building the Design System.",
+            "Collaborate closely with the development team to implement enhancements.",
+            "Successfully implementing user experience optimization based on user feedback.",
+            "Participate in the development and implementation of new features to enhance the product.",
+            "Participate in identifying and designing the user interface and user experience for the new product."
+        ],
+    },
+    {
+        title: "UI UX Design",
+        company_name: "Panic Bear Co., Ltd",
+        icon: bearplus,
+        iconBg: "#E2E3E4",
+        date: "July 2022 - Present",
+        points: [
+            "Developing user interface and product experience based on customer requirements.",
+            "Participated in creating wireframes and interactive charts.",
+            "Participated in creating wireframes and prototypes to present concepts and interactions.",
+        ],
+    },
+];
+
+export const socialLinks = [
+    {
+        name: 'Contact',
+        iconUrl: contact,
+        link: '/contact',
+    },
+    {
+        name: 'GitHub',
+        iconUrl: github,
+        link: 'https://github.com/draconguyen123',
+    },
+    {
+        name: 'LinkedIn',
+        iconUrl: linkedin,
+        link: 'https://www.linkedin.com/in/nguyen-hoang-long-467850224/',
+    }
+];
+
+export const projects = [
+    {
+        iconUrl: tdental,
+        name: 'TDental - Dental Management Software - Web + Mobile',
+        description: 'The dental clinic management software project is a software system developed to enhance the management and performance of dental clinics.',
+        link: 'https://drive.google.com/file/d/1a9cm99mUKvODtjBb5QkSCj-d_ZVHumpC/view?usp=sharing',
+    },
+    {
+        iconUrl: tpos,
+        name: 'TPage - Fanpage Management Software',
+        description: 'The sales management software project on fanpages combines management and advertising features on fanpages across social media...',
+        link: 'https://drive.google.com/file/d/1qWbNdIrETZ7AP-6cvp27aCmQRwg7sg7i/view?usp=sharing',
+    },
+    {
+        iconUrl: sensorflow,
+        name: 'SensorFlow - Hotel Sensor Management Software',
+        description: 'This project allows monitoring and control of parameters such as lighting, temperature, humidity, and in-room services...',
+        link: 'https://drive.google.com/file/d/14LR2WPkuGz-cYX1LjA7yuFM4SUtk2ecV/view?usp=sharing',
+    },
+    {
+        iconUrl: bluecarbon,
+        name: 'Blue Carbon Management Software',
+        description: 'Blue Carbon Management Software Project" is an application system designed for monitoring, managing, and analyzing data related to the CO2... ',
+        link: 'https://drive.google.com/file/d/1POzofwbT8ZsMfpVdTTyGL_9AokjhyStl/view?usp=sharing',
+    },
+    {
+        iconUrl: ybl,
+        name: 'YBL - Financial Insurance Advisory Software',
+        description: 'The "Financial Insurance Consultation Software Project" is an application designed for financial advisors and customers to manage and optimize...',
+        link: 'https://drive.google.com/file/d/1Is-7_sUu4TpeyV2eseNQY417cB4v3IfT/view?usp=sharing',
+    },
+    {
+        iconUrl: long,
+        name: '1LONG Accumulative Investment Mobile App',
+        description: 'Investment Accumulation Mobile App Project helps users manage and invest their personal finances, providing information and tools...',
+        link: 'https://drive.google.com/file/d/1VIJsAF0KONEJ0Mrdoj0hbl_6XoJdI0Ts/view?usp=sharing',
+    }
+];
